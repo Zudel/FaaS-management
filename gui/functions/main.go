cc88c230369dcfe7461e9ce6d040c14172e2a337
Roberto <roberto.fard@gmail.com>
2023-07-19 17:26:01 +0200

    python Gui

---
+++ gui/functions/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func countOccurrences(input string) []int {
+	occurrences := make([]int, 26) // Creazione di un array di interi per le 26 lettere dell'alfabeto inglese
+
+	for _, char := range input {
+		if char >= 'a' && char <= 'z' {
+			occurrences[char-'a']++ // Incremento l'occorrenza corrispondente all'indice della lettera nell'array
+		} else if char >= 'A' && char <= 'Z' {
+			occurrences[char-'A']++ // Incremento l'occorrenza corrispondente all'indice della lettera nell'array
+		}
+	}
+
+	return occurrences
+}
+func power(x int, y int) int {
+	result := 1
+	for i := 0; i < y; i++ {
+		result *= x
+	}
+	return result
+}
+
+func main() {
+	var x, y int
+	var input string
+	x = 2
+	y = 3
+	input = "Hello, World!"
+	result := power(x, y)
+	println(result)
+	power(x, y)
+	countOccurrences(input)
+	fmt.Println(countOccurrences(input))
+}
--- gui/main.py
+++ gui/main.py
@@ -1,28 +1,34 @@
-#crea una gui per la gestione dei file
-#crea una gui per la gestione dei file
 
-import boto3
 import tkinter as tk
+import subprocess
 
-#devo creare una gui per le funzioi 
-
-def on_button_click():
-    label.config(text="Button clicked!")
 
 # Creazione dell'applicazione GUI
 app = tk.Tk()
 app.title("FaaS Management GUI")
-app.geometry("800x600")
+app.geometry("720x480")
 
+def on_button_click():
+    #invocare una funzione scritta in Go
+    process = subprocess.run(["./functions"],capture_output=True, text=True)
+    output = process.stdout.read()
+    print(output)
+    
 
-# Creazione di un pulsante
-button = tk.Button(app, text="Click Me", command=on_button_click)
-button.pack()
 
 # Creazione di una etichetta per visualizzare il risultato
 label = tk.Label(app, text="Hello, world!")
-label.pack()
+label.grid(row=0, column=0)
+tk.Label(app, text="Hello, world!").grid(row=2, column=0)
+tk.Label(app, text="this is una cosa").grid(row=3, column=0)
 
+#text input 
+e1 = tk.Entry(app, width=50, borderwidth=5).grid(row=2, column=1)
+e2 = tk.Entry(app, width=50, borderwidth=5).grid(row=3, column=1)
+# Creazione di un pulsante
+button = tk.Button(app, text="Click Me", command=on_button_click).grid(row=4, column=5)
+def on_button_click():
+   button.config(text="Button clicked!")
 # Esecuzione dell'applicazione
 app.mainloop()
 
